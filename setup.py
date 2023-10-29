895325f036fe51e3003aefba62b026e5958b42f7
toast-uz <tatsuzo.osawa@gmail.com>
2023-10-29 16:44:26 +0900

    1st commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!python
+# flake8: noqa
+
+# AHC環境セットアップ用スクリプト
+# 冪等性あり（複数回実行してもよい）
+
+import os
+
+# toolsディレクトリが無ければ作成を指示して終了する
+if not os.path.isdir('tools'):
+    print('toolsディレクトリが無いので作成してください')
+    exit(1)
+print('Found tools directory.')
+
+# .gitignoreが無ければ作成する
+print('Creating .gitignore...')
+with open('.gitignore', 'w') as f:
+    f.write('''testcases/
+tools/
+Cargo.lock
+.vscode/
+eval.py
+setup.py
+rust-toolchain
+vis.html
+''')
+
+# rust-toolchainを作成する
+print('Creating rust-toolchain...')
+with open('rust-toolchain', 'w') as f:
+    f.write('1.70.0\n')
+
+# tools/inが無ければ作成する
+if not os.path.isdir('tools/in'):
+    print('Creating tools/in...')
+    os.mkdir('tools/in')
+else:
+    print('Found tools/in.')
+
+# toolsのツール群をコンパイルする
+print('Compiling tools...')
+os.chdir('tools')
+os.system('cargo build --release')
+os.chdir('..')
