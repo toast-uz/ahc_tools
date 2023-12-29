d79321f6d7ecbc821cfe864990cc0567ffec9466
toast-uz <tatsuzo.osawa@gmail.com>
2023-12-29 20:12:50 +0900

    Add download ahc-standings

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 #!python
-# flake8: noqa
 
 # AHC環境セットアップ用スクリプト
+# Copyright (c) 2023 toast-uz
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
 # 冪等性あり（複数回実行してもよい）
 
 import os
 
-# toolsディレクトリが無ければ作成を指示して終了する
-if not os.path.isdir('tools'):
-    print('toolsディレクトリが無いので作成してください')
-    exit(1)
-print('Found tools directory.')
-
-# .gitignoreが無ければ作成する
-print('Creating .gitignore...')
-with open('.gitignore', 'w') as f:
-    f.write('''testcases/
+GITIGNORE = '''testcases/
 tools/
 Cargo.lock
 .vscode/
@@ -23,22 +17,39 @@ eval.py
 setup.py
 rust-toolchain
 vis.html
-''')
+'''
+RUST_TOOLCHAIN = '1.70.0'
+AHC_STANDINGS_URL = 'https://img.atcoder.jp/ahc_standings/index.html'
 
-# rust-toolchainを作成する
-print('Creating rust-toolchain...')
-with open('rust-toolchain', 'w') as f:
-    f.write('1.70.0\n')
+def create_if_not_exists(path, type_='file', content=''):
+    if type_ == 'file' and os.path.isfile(path) or type_ == 'dir' and os.path.isdir(path):
+        print(f'{path} already exists.')
+        return
+    print(f'Creating {type_} at {path} ...')
+    if type_ == 'file':
+        with open(path, 'w') as f:
+            f.write(content)
+    elif type_ == 'dir':
+        os.mkdir(path)
 
-# tools/outが無ければ作成する
-if not os.path.isdir('tools/out'):
-    print('Creating tools/out...')
-    os.mkdir('tools/out')
-else:
-    print('Found tools/out.')
+def main():
+    # toolsディレクトリが無ければ作成を指示して終了する
+    if not os.path.isdir('tools'):
+        print('toolsディレクトリが無いので作成してください')
+        exit(1)
+    print('Found tools directory.')
+    # 必要なファイルやディレクトリを作成する
+    create_if_not_exists('.gitignore', 'file', GITIGNORE)
+    create_if_not_exists('rust-toolchain', 'file', RUST_TOOLCHAIN)
+    create_if_not_exists('tools/out', 'dir')
+    # toolsのツール群をコンパイルする
+    print('Compiling tools...')
+    os.chdir('tools')
+    os.system('cargo build --release')
+    os.chdir('..')
+    # AtCoder Standingsをダウンロードする
+    print('Downloading AtCoder Standings...')
+    os.system(f'curl {AHC_STANDINGS_URL} -o tools/out/index.html')
 
-# toolsのツール群をコンパイルする
-print('Compiling tools...')
-os.chdir('tools')
-os.system('cargo build --release')
-os.chdir('..')
+if __name__ == '__main__':
+    main()
