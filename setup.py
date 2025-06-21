59eb35b9cb940681c8e52b2d688533444f7bf60d
toast-uz <tatsuzo.osawa@gmail.com>
2025-06-21 12:36:58 +0900

    Added auto download and setup tools dir

--- setup.py
+++ setup.py
@@ -18,6 +18,10 @@ vis.html
 RUST_TOOLCHAIN = '1.70.0'
 AHC_STANDINGS_URL = 'https://img.atcoder.jp/ahc_standings/index.html'
 
+def get_contest_name():
+    """コンテスト名を取得する"""
+    return os.path.basename(os.getcwd())
+
 def create_if_not_exists(path, type_='file', content=''):
     if type_ == 'file' and os.path.isfile(path) or type_ == 'dir' and os.path.isdir(path):
         print(f'{path} already exists.')
@@ -30,6 +34,35 @@ def create_if_not_exists(path, type_='file', content=''):
         os.mkdir(path)
 
 def main():
+    # 問題をダウンロードする
+    contest_name = get_contest_name()
+    print('Downloading problem ...')
+    os.system(f'curl https://atcoder.jp/contests/{contest_name}/tasks/{contest_name}_a -o problem.html')
+    # 問題を読み込んで.zipのファイルパスを取得する
+    if not os.path.isfile('problem.html'):
+        print('Problem file not found. Please check the contest name or the URL.')
+        exit(1)
+    with open('problem.html', 'r') as f:
+        content = f.read()
+    zip_path = None
+    for line in content.splitlines():
+        if 'href' in line and '.zip' in line:
+            zip_path = line.split('"')[1]
+            break
+    if not zip_path:
+        print('No zip file found in the problem page. Please check the contest name or the URL.')
+        exit(1)
+    print(f'Found zip file: {zip_path}')
+    # zipファイルをダウンロードする
+    os.system(f'curl {zip_path} -o downloaded_tools.zip')
+    # zipファイルを解凍する
+    if not os.path.isfile('downloaded_tools.zip'):
+        print('Testcases zip file not found. Please check the contest name or the URL.')
+        exit(1)
+    print('Unzipping testcases.zip ...')
+    os.system('unzip -o downloaded_tools.zip -d .')
+    # ダウンロードしたzipファイルを削除する
+    os.remove('downloaded_tools.zip')
     # toolsディレクトリが無ければ作成を指示して終了する
     if not os.path.isdir('tools'):
         print('toolsディレクトリが無いので作成してください')
